458bf59693a903d8a404a5ec13056fd4f8cdb257
Josué Ruiz <sistemas@promoser.com.mx>
2022-05-13 18:58:20 -0500

    chapter 5 - updates

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+/*
+const message = document.querySelector('.message');
+const number = document.querySelector('.number');
+const score = document.querySelector('.score');
+const guess = document.querySelector('.guess');
+
+console.log(message.textContent);
+
+message.textContent = 'Correct number!!';
+number.textContent = '34543';
+score.textContent = '54543543543';
+guess.value = '4';
+
+console.log(typeof message);*/
+
+const btn = document.querySelector('.check');
+
+btn.addEventListener('click', function() {
+  const guess = Number(document.querySelector('.guess').value);
+  const message = document.querySelector('.message');
+
+  if (!guess) {
+    message.textContent = 'Not a number!!';
+  } else {
+    message.textContent = 'Try another number';
+  }
+});
