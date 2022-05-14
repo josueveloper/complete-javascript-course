7695d95126c121bc264c47567644ccdc1ccdf081
Josué Ruiz <sistemas@promoser.com.mx>
2022-05-14 14:02:51 -0500

    udpates

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -14,15 +14,63 @@ guess.value = '4';
 
 console.log(typeof message);*/
 
-const btn = document.querySelector('.check');
+const numberToGuessElement = document.querySelector('.number');
+const scoreElement = document.querySelector('.score');
+const message = document.querySelector('.message');
+const guessElement = document.querySelector('.guess');
 
-btn.addEventListener('click', function() {
-  const guess = Number(document.querySelector('.guess').value);
-  const message = document.querySelector('.message');
+let numberToGuess = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+numberToGuessElement.textContent = numberToGuess;
+scoreElement.textContent = score;
+
+const btnCheck = document.querySelector('.check');
+const btnAgain = document.querySelector('.again');
+
+const gameOver = function() {
+  message.textContent = 'You lose!!';
+  btnCheck.disabled = true;
+};
+
+const youWin = function() {
+  message.textContent = 'You win!!';
+  btnCheck.disabled = true;
+};
+
+const newGame = function() {
+  message.textContent = 'Start guessing...';
+  btnCheck.disabled = false;
+  numberToGuess = Math.trunc(Math.random() * 20) + 1;
+  numberToGuessElement.textContent = numberToGuess;
+  score = 20;
+  scoreElement.textContent = score;
+  guessElement.value = '';
+};
+
+btnCheck.addEventListener('click', function() {
+  const guess = Number(guessElement.value);
 
   if (!guess) {
     message.textContent = 'Not a number!!';
+  } else if (guess === numberToGuess) {
+    youWin();
   } else {
-    message.textContent = 'Try another number';
+    message.textContent = 'Try another one';
+    score--;
+    scoreElement.textContent = score;
+
+    if (score === 0) {
+      gameOver();
+      return;
+    } else if (guess > numberToGuess) {
+      message.textContent = 'Too high';
+    } else {
+      message.textContent = 'Too low';
+    }
   }
+});
+
+btnAgain.addEventListener('click', function() {
+  newGame();
 });
