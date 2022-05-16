8024f32401330ae017efec16f4e1cf5e6040f4e7
Josué Ruiz <sistemas@promoser.com.mx>
2022-05-16 14:19:59 -0500

    chapter 6 - udpates

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModel = function() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(elem => {
+  elem.addEventListener('click', openModal);
+});
+btnCloseModal.addEventListener('click', closeModel);
+overlay.addEventListener('click', closeModel);
+
+//pendiente
+document.addEventListener('keydown', closeModel);
